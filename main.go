def1b51392c871d4beda07abe7e312bf4d5d9695
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2017-11-15 10:02:48 +0530

    Read version from build flags

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"github.com/sebdah/markdown-toc/cmd"
 )
 
+var version = "undefined"
+
 func main() {
+	cmd.Version = version
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
